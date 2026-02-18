69d01ec190fecaa40177391da2efe09dffd5ab75
NikiRaykov <79482102+NikiRaykov@users.noreply.github.com>
2026-02-18 23:57:35 +0100

    Add files via upload

---
+++ problems/Train.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Train{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int wagons = Integer.parseInt(scanner.nextLine());
+
+        int[] train = new int[wagons];
+
+        for (int i = 0; i < wagons; i++) {
+            train[i] = Integer.parseInt(scanner.nextLine());
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i < train.length; i++) {
+            System.out.print(train[i] + " ");
+            sum += train[i];
+        }
+        System.out.println();
+        System.out.println(sum);
+    }
+}
